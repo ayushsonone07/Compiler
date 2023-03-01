8e16034091bb99ca628fe9d665224591c9622c9f
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2023-03-01 13:10:40 +0000

    add more files

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ let editor = CodeMirror.fromTextArea(document.getElementById("editor"),{
 
 let width = window.innerWidth
 editor.setSize(0.7*width, "500")
+
+let option = document.getElementById("inlineFormSelectPref")
+option.addEventListener("change",function(){
+    if(option.value == Java){
+        editor.setOption("mode","text/x-java")
+    }
+    else if(option.value == Python){
+        editor.setOption("mode","text/x-java")
+    }
+    else{
+        editor.setOption("mode","text/x-c++src")
+    }
+})
