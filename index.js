218f09d56cac9e1472601f0df899bd2ee8d94459
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2023-03-01 06:19:04 +0000

    edited files

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let editor = CodeMirror.fromTextArea(document.getElementById("editor"),{
-    mode: "text/x-c++",
-    theme : 
+    mode: "text/x-c++src",
+    theme : "dracula",
+    lineNumbers : true,
+    autoCloseBracket : true
 })
+
+let width = window.innerWidth
+editor.setSize(0.7*width, "500")
