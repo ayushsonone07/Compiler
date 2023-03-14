20b2bb9fa381cadc489c99a741d4c7aaa66c7572
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2023-03-14 04:18:55 +0000

    added changes

--- api.js
+++ api.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const app = express()
-const bodyParser = require("body-parser")
-app.use(bodyParser.json())
+const bodyP = require("body-parser")
+const compiler = require("compilex")
+const option = {stats : true}
+compiler.init(option)
+
+app.use(bodyP.json())
 app.use("/codemirror-5.65.12", express.static("/workspaces/Compiler/codemirror-5.65.12"))
-app.use("/",function(req,res){
+app.get("/",function(req,res){
     res.sendFile("/workspaces/Compiler/index.html")
 })
+app.post("/compile", function(req,res){
+    var code = req.body.code
+    var input = req.body.input
+    var lang = req.body.lang
+    var envData = { OS : "linux" }; 
+    compiler.compilePython( envData , code , function(data){
+        res.send(data);
+    });
+})
+
 app.listen(8000)
